0eb5e58323b848b5ca17d9b1cf4e4011cdbf5e35
ravikumar <kp.ravikumar04@gmail.com>
2025-03-06 23:00:10 +0530

    initial commit

---
+++ src/main/java/com/awsravi/javay25/ModranJava8Application.java
@@ -0,0 +1,13 @@
+package com.awsravi.javay25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModranJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModranJava8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/awsravi/javay25/ModranJava8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awsravi.javay25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModranJava8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
